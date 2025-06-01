de656f973737eab0643533f92a91dfb949c371c4
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-06-01 19:21:42 +0900

    fixing font_setup

--- font_setup/font_setup.cpp
+++ font_setup/font_setup.cpp
@@ -34,26 +34,25 @@ WCHAR JF_LocalName1[] = {0xFF2D, 0xFF33, ' ', 0xFF30, 0x660E, 0x671D, 0};       
 WCHAR JF_LocalName2[] = {0xFF2D, 0xFF33, ' ', 0x30B4, 0x30B7, 0x30C3, 0x30AF, 0};           // MS Gothic
 WCHAR JF_LocalName3[] = {0xFF2D, 0xFF33, ' ', 0xFF30, 0x30B4, 0x30B7, 0x30C3, 0x30AF, 0};   // MS PGothic
 
-static const FONTSUBST NEU_MapForInstall[] =
-{
-    { L"MS Mincho",       NULL },
-    { L"MS Gothic",       NULL },
-    { L"MS PMincho",      NULL },
-    { L"MS PGothic",      NULL },
-    { L"MS UI Gothic",    NULL },
-};
-
 static const FONTSUBST JPN_MapForInstall[] =
 {
-    { JF_LocalName0,      NULL },
-    { JF_LocalName2,      NULL },
-    { JF_LocalName1,      NULL },
-    { JF_LocalName3,      NULL },
+    { L"MS Mincho",       JF_LocalName0 },
+    { L"MS PMincho",      JF_LocalName1 },
+    { L"MS Gothic",       JF_LocalName2 },
+    { L"MS PGothic",      JF_LocalName3 },
+    { JF_LocalName0,      JF_LocalName0 },
+    { JF_LocalName1,      JF_LocalName1 },
+    { JF_LocalName2,      JF_LocalName2 },
+    { JF_LocalName3,      JF_LocalName3 },
+    { L"MS UI Gothic",    L"MS UI Gothic" },
+    { L"MS UI Gothic 2",  L"MS UI Gothic" },
+    { L"MS Shell Dlg",    L"MS UI Gothic" },
+    { L"MS Shell Dlg 2",  L"MS UI Gothic" },
     { L"Tahoma",          L"MS UI Gothic" },
     { L"System",          L"MS UI Gothic" },
 };
 
-static const FONTSUBST NEU_MapForUninstallNoDroid[] =
+static const FONTSUBST JPN_MapForUninstallNoDroid[] =
 {
     { L"MS UI Gothic",    NULL },
     { L"MS Mincho",       NULL },
@@ -61,38 +60,31 @@ static const FONTSUBST NEU_MapForUninstallNoDroid[] =
     { L"MS Gothic",       NULL },
     { L"MS PGothic",      NULL },
     { L"MS UI Gothic",    NULL },
-    { L"System",          NULL },
-    { L"Tahoma",          NULL },
-};
-
-static const FONTSUBST JPN_MapForUninstallNoDroid[] =
-{
+    { L"MS UI Gothic 2",  NULL },
     { JF_LocalName0,      NULL },
     { JF_LocalName1,      NULL },
     { JF_LocalName2,      NULL },
     { JF_LocalName3,      NULL },
-    { L"System",          NULL },
-    { L"Tahoma",          NULL },
-};
-
-static const FONTSUBST NEU_MapForUninstallWithDroid[] =
-{
-    { L"MS UI Gothic",    L"Droid Sans Fallback" },
-    { L"MS Mincho",       L"Droid Sans Fallback" },
-    { L"MS PMincho",      L"Droid Sans Fallback" },
-    { L"MS Gothic",       L"Droid Sans Fallback" },
-    { L"MS PGothic",      L"Droid Sans Fallback" },
-    { L"MS UI Gothic",    L"Droid Sans Fallback" },
+    { L"MS Shell Dlg",    NULL },
+    { L"MS Shell Dlg 2",  NULL },
     { L"System",          NULL },
     { L"Tahoma",          NULL },
 };
 
 static const FONTSUBST JPN_MapForUninstallWithDroid[] =
 {
+    { L"MS Mincho",       L"Droid Sans Fallback" },
+    { L"MS PMincho",      L"Droid Sans Fallback" },
+    { L"MS Gothic",       L"Droid Sans Fallback" },
+    { L"MS PGothic",      L"Droid Sans Fallback" },
+    { L"MS UI Gothic",    L"Droid Sans Fallback" },
+    { L"MS UI Gothic 2",  L"Droid Sans Fallback" },
     { JF_LocalName0,      L"Droid Sans Fallback" },
     { JF_LocalName1,      L"Droid Sans Fallback" },
     { JF_LocalName2,      L"Droid Sans Fallback" },
     { JF_LocalName3,      L"Droid Sans Fallback" },
+    { L"MS Shell Dlg",    L"Droid Sans Fallback" },
+    { L"MS Shell Dlg 2",  L"Droid Sans Fallback" },
     { L"System",          L"Droid Sans Fallback" },
     { L"Tahoma",          L"Droid Sans Fallback" },
 };
@@ -142,10 +134,12 @@ BOOL DoMakeUserJapanese(HWND hwnd)
     DoSetLocale(TRUE, lcid, L"932", L"932");
 
     MRegKey keyConsole;
-    keyConsole.RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Console"),
-        0, KEY_WRITE | KEY_ENUMERATE_SUB_KEYS);
-    keyConsole.SetSz(L"FaceName", L"MS Gothic");
-    keyConsole.SetDword(L"CodePage", 932);
+    if (keyConsole.RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Console"),
+                                0, KEY_WRITE | KEY_ENUMERATE_SUB_KEYS) == NOERROR)
+    {
+        keyConsole.SetSz(L"FaceName", L"MS Gothic");
+        keyConsole.SetDword(L"CodePage", 932);
+    }
 
     for (DWORD dwIndex = 0; ; ++dwIndex)
     {
@@ -155,9 +149,11 @@ BOOL DoMakeUserJapanese(HWND hwnd)
             break;
 
         MRegKey key;
-        key.RegOpenKeyEx(keyConsole, szName, 0, KEY_WRITE);
-        key.SetSz(L"FaceName", L"MS Gothic");
-        key.SetDword(L"CodePage", 932);
+        if (key.RegOpenKeyEx(keyConsole, szName, 0, KEY_WRITE) == NOERROR)
+        {
+            key.SetSz(L"FaceName", L"MS Gothic");
+            key.SetDword(L"CodePage", 932);
+        }
     }
 
     return TRUE;
@@ -179,10 +175,12 @@ BOOL DoMakeUserEnglish(HWND hwnd)
     DoSetLocale(TRUE, lcid, L"1252", L"437");
 
     MRegKey keyConsole;
-    keyConsole.RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Console"),
-        0, KEY_WRITE | KEY_ENUMERATE_SUB_KEYS);
-    keyConsole.SetSz(L"FaceName", L"VGA");
-    keyConsole.SetDword(L"CodePage", 437);
+    if (keyConsole.RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Console"),
+                                0, KEY_WRITE | KEY_ENUMERATE_SUB_KEYS) == NOERROR)
+    {
+        keyConsole.SetSz(L"FaceName", L"VGA");
+        keyConsole.SetDword(L"CodePage", 437);
+    }
 
     for (DWORD dwIndex = 0; ; ++dwIndex)
     {
@@ -192,9 +190,11 @@ BOOL DoMakeUserEnglish(HWND hwnd)
             break;
 
         MRegKey key;
-        key.RegOpenKeyEx(keyConsole, szName, 0, KEY_WRITE);
-        key.SetSz(L"FaceName", L"VGA");
-        key.SetDword(L"CodePage", 437);
+        if (key.RegOpenKeyEx(keyConsole, szName, 0, KEY_WRITE) == NOERROR)
+        {
+            key.SetSz(L"FaceName", L"VGA");
+            key.SetDword(L"CodePage", 437);
+        }
     }
 
     return TRUE;
@@ -467,7 +467,6 @@ WinMain(HINSTANCE   hInstance,
 
     if (lstrcmpiA(lpCmdLine, "/i") == 0) // install
     {
-        DoSetupSubst(NEU_MapForInstall);
         DoMakeUserJapanese(NULL);
         DoSetupSubst(JPN_MapForInstall);
         DoSetUserKeyboardRegistry(1, 0x411, 0x409, TRUE);
@@ -475,14 +474,7 @@ WinMain(HINSTANCE   hInstance,
         DoNotepadFont(TRUE);
         DoSetupConsoleFonts(TRUE);
 
-        if (IsThereDroidFont())
-        {
-            MoveFileEx(GetDroidFontPath(), GetDroidFontBackupPath(),
-                       MOVEFILE_DELAY_UNTIL_REBOOT);
-        }
-
-        //SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
-
+        PostMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
         return 0;
     }
     else if (lstrcmpiA(lpCmdLine, "/u") == 0) // uninstall
@@ -491,11 +483,11 @@ WinMain(HINSTANCE   hInstance,
 
         if (IsThereDroidFont())
         {
-            DoSetupSubst(NEU_MapForUninstallWithDroid);
+            DoSetupSubst(JPN_MapForUninstallWithDroid);
         }
         else
         {
-            DoSetupSubst(NEU_MapForUninstallNoDroid);
+            DoSetupSubst(JPN_MapForUninstallNoDroid);
         }
 
         if (IsUserJapanese())
@@ -515,14 +507,7 @@ WinMain(HINSTANCE   hInstance,
 
         DoSetupConsoleFonts(FALSE);
 
-        if (IsThereDroidFontBackup())
-        {
-            MoveFileEx(GetDroidFontBackupPath(), GetDroidFontPath(),
-                       MOVEFILE_DELAY_UNTIL_REBOOT);
-        }
-
-        //SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
-
+        PostMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
         return 0;
     }
     else
